6082c6804487257aefb9b3c22ab9b16d49758725
raghork <michele.argenti000@gmail.com>
2025-07-19 12:58:51 +0200

    New cahnges

--- src/js/product.js
+++ src/js/product.js
@@ -22,10 +22,14 @@ document
   .addEventListener("click", addToCartHandler);
 */
 
-import { setLocalStorage, getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData('tents');
-const productId = getParam('product');
+loadHeaderFooter();
 
-console.log(dataSource.findProductById(productId));
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
